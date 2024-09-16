9a7a7a996ef683e0d1e4cb1319e9ffd2382e618f
otaviodbueno <otaviodamo15@gmail.com>
2024-09-15 23:31:47 -0300

    Exercicio Concluido

---
+++ Entities/ImportedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double totalPrice()
+        {
+            return Price + CustomsFee;
+        }
+        public override string PriceTag()
+        {
+            return Name + " $" + totalPrice() +
+            " | Customs fee: $" + CustomsFee;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curso.Entities;
+using Curso.Entities;
 
 
-Console.WriteLine("Teste");
-Console.WriteLine("Testando");
+List<Product> products = new List<Product>();
 
+Console.WriteLine("Enter the numer of products: ");
+int number = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Oláááááa´´aá");
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine($"Product {i + 1} data: ");
+    Console.WriteLine("Common, used or imported? (c/u/i)");
+    string typeOfProduct = Console.ReadLine();
+
+    if (typeOfProduct == "c")
+    {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Price: ");
+        double price = double.Parse(Console.ReadLine());
+
+        Product product = new Product(name, price);
+
+        products.Add(product);
+    }
+
+    if (typeOfProduct == "u")
+    {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Price: ");
+        double price = double.Parse(Console.ReadLine());
+        Console.Write("Manufacture date (DD/MM/YYYY): ");
+        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+        UsedProduct product = new UsedProduct(name, price, manufactureDate);
+
+        products.Add(product);
+    }
+
+    if (typeOfProduct == "i")
+    {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Price: ");
+        double price = double.Parse(Console.ReadLine());
+        Console.Write("Customs Fee: ");
+        double customsFee = double.Parse(Console.ReadLine());
+
+        ImportedProduct product = new ImportedProduct(name, price, customsFee); git
+
+        products.Add(product);
+    }
+}
+
+Console.WriteLine("PRICE TAGS:");
+
+foreach (Product product in products)
+{
+    Console.WriteLine(product.PriceTag());
+}
+
